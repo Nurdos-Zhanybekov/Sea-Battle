8c3ea96dc240591d3d2d2e01b6fc34a0ad386ece
Nurdos Zhanybekov <nurdoszhanybekov.w@gmail.com>
2025-01-03 19:48:31 +0600

    added map and random location of ships

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,35 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+        greeting();
 
+        int rows_number = 7;
+        int columns_number = 7;
+        int[][] show_map = map(rows_number, columns_number);
 
+        for (int i = 0; i < rows_number; i++){
+            for (int j = 0; j < columns_number; j++){
+                System.out.print(show_map[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void greeting(){
+        System.out.println("Sea-Battle");
+    }
+
+    public static int[][] map(int rows_number, int columns_number){
+        Random random = new Random();
+        int[][] fill_map = new int[rows_number][columns_number];
+
+        for(int i = 0; i < rows_number; i++){
+            for(int j = 0; j < columns_number; j++){
+                fill_map[i][j] = random.nextInt(0, 2);
+            }
+        }
+
+        return fill_map;
     }
 }
