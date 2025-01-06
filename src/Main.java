820e53c60794acb8c2e07bf6eafed203dcab65ab
Nurdos Zhanybekov <nurdoszhanybekov.w@gmail.com>
2025-01-06 21:10:24 +0600

    now 2 and 3 cells ships place randomly

--- src/Main.java
+++ src/Main.java
@@ -23,13 +23,44 @@ public class Main {
     public static int[][] map(int rows_number, int columns_number){
         Random random = new Random();
         int[][] fill_map = new int[rows_number][columns_number];
+        int random_row = random.nextInt(rows_number);
+        int random_column = random.nextInt(columns_number);
+        boolean horizontal_placing = random.nextBoolean();
 
-        for(int i = 0; i < rows_number; i++){
-            for(int j = 0; j < columns_number; j++){
-                fill_map[i][j] = random.nextInt(0, 2);
-            }
-        }
+        placeShip(fill_map, random_row, random_column, 3, horizontal_placing, rows_number, columns_number, random);
+        random_row = random.nextInt(rows_number);
+        random_column = random.nextInt(columns_number);
+        horizontal_placing = random.nextBoolean();
+
+        placeShip(fill_map, random_row, random_column, 2, horizontal_placing, rows_number, columns_number, random);
+        random_row = random.nextInt(rows_number);
+        random_column = random.nextInt(columns_number);
+        horizontal_placing = random.nextBoolean();
+
+        placeShip(fill_map, random_row, random_column, 2, horizontal_placing, rows_number, columns_number, random);
+        random_row = random.nextInt(rows_number);
+        random_column = random.nextInt(columns_number);
+        horizontal_placing = random.nextBoolean();
+
 
         return fill_map;
+    }
+
+    public static void placeShip(int[][] map, int start_row, int start_column, int size, boolean horizontal, int rows, int columns, Random random) {
+        while (start_row + size > rows && start_column + size > columns){
+            start_row = random.nextInt(rows);
+            start_column = random.nextInt(columns);
+            horizontal = random.nextBoolean();
+        }
+
+        if (horizontal) {
+            for (int i = 0; i < size; i++){
+                map[start_row][start_column + i] = 1;
+            }
+        } else{
+            for (int i = 0; i < size; i++){
+                map[start_row + i][start_column] = 1;
+            }
+        }
     }
 }
