321b364cb6bb766475ea62c288e7ba287e3be24e
Nurdos Zhanybekov <nurdoszhanybekov.w@gmail.com>
2025-01-07 15:17:56 +0600

    now there is space between ships

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         greeting();
 
         int rows_number = 7;
@@ -23,44 +23,77 @@ public class Main {
     public static int[][] map(int rows_number, int columns_number){
         Random random = new Random();
         int[][] fill_map = new int[rows_number][columns_number];
-        int random_row = random.nextInt(rows_number);
-        int random_column = random.nextInt(columns_number);
-        boolean horizontal_placing = random.nextBoolean();
 
-        placeShip(fill_map, random_row, random_column, 3, horizontal_placing, rows_number, columns_number, random);
-        random_row = random.nextInt(rows_number);
-        random_column = random.nextInt(columns_number);
-        horizontal_placing = random.nextBoolean();
+        placeShip(fill_map, 3, rows_number, columns_number, random);
 
-        placeShip(fill_map, random_row, random_column, 2, horizontal_placing, rows_number, columns_number, random);
-        random_row = random.nextInt(rows_number);
-        random_column = random.nextInt(columns_number);
-        horizontal_placing = random.nextBoolean();
+        placeShip(fill_map, 2, rows_number, columns_number, random);
+        placeShip(fill_map, 2, rows_number, columns_number, random);
 
-        placeShip(fill_map, random_row, random_column, 2, horizontal_placing, rows_number, columns_number, random);
-        random_row = random.nextInt(rows_number);
-        random_column = random.nextInt(columns_number);
-        horizontal_placing = random.nextBoolean();
-
+        placeShip(fill_map, 1, rows_number, columns_number, random);
+        placeShip(fill_map, 1, rows_number, columns_number, random);
+        placeShip(fill_map, 1, rows_number, columns_number, random);
+        placeShip(fill_map, 1, rows_number, columns_number, random);
 
         return fill_map;
     }
 
-    public static void placeShip(int[][] map, int start_row, int start_column, int size, boolean horizontal, int rows, int columns, Random random) {
-        while (start_row + size > rows && start_column + size > columns){
-            start_row = random.nextInt(rows);
-            start_column = random.nextInt(columns);
-            horizontal = random.nextBoolean();
+    public static void placeShip(int[][] map, int size, int rows, int columns, Random random){
+        boolean placed = false;
+
+        while(!placed) {
+            int random_row = random.nextInt(rows);
+            int random_column = random.nextInt(columns);
+            boolean horizontal_placing = random.nextBoolean();
+
+            if (horizontal_placing) {
+                if(random_column + size <= columns && noShipsAround(map, random_row, random_column, size, rows, columns, true)) {
+                    for (int i = 0; i < size; i++) {
+                        map[random_row][random_column + i] = 1;
+                    }
+
+                    placed = true;
+                }
+            } else {
+                if(random_row + size <= rows && noShipsAround(map, random_row, random_column, size, rows, columns, false)) {
+                    for (int i = 0; i < size; i++) {
+                        map[random_row + i][random_column] = 1;
+                    }
+
+                    placed = true;
+                }
+            }
+        }
+    }
+
+    public static boolean noShipsAround(int[][] map, int row, int column, int size, int rows, int columns, boolean horizontal){
+        if(horizontal){
+            if(column + size > columns) return false;
+            
+            for(int i = -1; i <= 1; i++){
+                for(int j = -1; j <= size + 1; j++){
+                    int r = row + i;
+                    int c = column + j;
+
+                    if(r >= 0 && r < rows && c >= 0 && c < columns && map[r][c] == 1){
+                        return false;
+                    }
+                }
+            }
+        }else{
+            if(row + size >= rows) return false;
+
+            for(int i = -1; i <= size + 1; i++){
+                for(int j = -1; j <= 1; j++){
+                    int r = row + i;
+                    int c = column + j;
+
+                    if(r >= 0 && r < rows && c >= 0 && c < columns && map[r][c] == 1){
+                        return false;
+                    }
+                }
+            }
         }
 
-        if (horizontal) {
-            for (int i = 0; i < size; i++){
-                map[start_row][start_column + i] = 1;
-            }
-        } else{
-            for (int i = 0; i < size; i++){
-                map[start_row + i][start_column] = 1;
-            }
-        }
+        return true;
     }
 }
